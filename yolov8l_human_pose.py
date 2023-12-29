a64a4c754dd1004a300bca653bd2dfeb0843b38a
Mr. Gulab Patel <62597299+gulabpatel@users.noreply.github.com>
2023-12-29 23:34:55 +0530

    Add files via upload

---
+++ yolov8l_human_pose.py
@@ -0,0 +1,37 @@
+import os
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from ultralytics import YOLO
+from pose_extract import Extrac_Pose
+
+model = YOLO('yolov8l-pose.pt')
+
+# video = cv2.VideoCapture('dancegirls.mp4')
+video = cv2.VideoCapture('danceboys.mp4')
+
+while video.isOpened():
+
+    ret, frame = video.read()
+
+    if ret:
+        frame = cv2.resize(frame, (600,400))
+        
+        results = model(frame)
+        anot_frame = results[0].plot()
+    
+        keypoints = results[0].keypoints  # Masks object
+
+        black_frame = Extrac_Pose(keypoints,frame)
+        
+        cv2.imshow("Original Video", anot_frame)
+        cv2.imshow("Extracted Pose", black_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+
+video.release()
+cv2.destroyAllWindows()
