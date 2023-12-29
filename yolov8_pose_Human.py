25e2f6425abfc075ce0d2ee32c8f17933419d69d
Mr. Gulab Patel <62597299+gulabpatel@users.noreply.github.com>
2023-12-29 23:42:07 +0530

    Delete yolov8_pose_Human.py

--- yolov8_pose_Human.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load a pretrained YOLOv8n model
-model = YOLO('yolov8m-pose.pt')
-#yolov8n-pose, yolov8s-pose, yolov8l-pose,yolov8x-pose,yolov8x-pose-p6
-
-# RUn inference on the source
-# results = model(source="gymnaste.mp4", show=True, conf=0.3, save=True)
-results = model(source=0, show=True, conf=0.3, save=True)
